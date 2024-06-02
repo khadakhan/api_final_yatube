ae8ff77c1c57010c51c25edbbe2f079c752b4d57
Konstantin Khadakhanov <k-project@mail.ru>
2024-06-02 15:41:20 +0300

    Second iteration

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
     'djoser',
     'posts.apps.PostsConfig',
