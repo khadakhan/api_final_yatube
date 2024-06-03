3717883244787148c79acc9bc22e167b57e0c6e9
Konstantin Khadakhanov <k-project@mail.ru>
2024-06-03 23:58:04 +0300

    Fifth commit

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -27,6 +27,9 @@ class Post(models.Model):
     def __str__(self):
         return self.text
 
+    class Meta:
+        ordering = ('pub_date',)
+
 
 class Comment(models.Model):
     author = models.ForeignKey(
